3ba7f53d07f575654865fa88cbeda090e5439327
sebasRT <rtsebas11@gmail.com>
2024-02-06 21:42:54 -0500

    Estructura base creada

---
+++ front-end/pi-2/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="grid justify-center">
+      <h1 className="text-center text-5xl">Proyecto Integrador 2</h1>
+    </main>
+  );
+}
