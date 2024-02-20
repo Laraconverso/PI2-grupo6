3024cf4691444a50121cb9f72e0b35dc18d31dee
Pablo Alvarez <pipualvarez1@gmail.com>
2024-02-20 20:57:33 -0300

    fix admin page

--- front-end/src/app/admin/page.tsx
+++ front-end/src/app/admin/page.tsx
@@ -1,27 +1,24 @@
 import { cookies } from "next/headers"
-import CreateUser from "./forms/CreateUser"
 import LoginAdmin from "./forms/LoginAdmin"
 import footballAdminBanner from "@public/bannerAdmin.png"
 import Image from "next/image"
 import ButtonViewForm from "./components/ButtonViewForm"
-import { useState } from "react"
-import CreateDT from "./forms/CreateDT"
 
 
 
-const page = () => {
+const page = async() => {
 
   const adminAuth = cookies().has("adminAuth");
 
   return (
     <main className="min-h-screen">
       {adminAuth ? (
-        <figure className="min-h-screen w-screen">
+        <figure className="w-full">
           <Image src={footballAdminBanner} className="object-cover" alt={"Football banner"}/>
         </figure>
       ): (undefined)}
       {adminAuth ? (
-        <main className="w-screen bg-primary-400 flex flex-col gap-5 items-center p-4 min-h-screen">
+        <main className="w-screen bg-primary-400 flex flex-col gap-5 items-center p-4 ">
           <div className={`mt-10`}>
             <h1 className="text-center text-5xl font-bauhs"> Bienvenido a ClubAPP</h1>
           </div>
