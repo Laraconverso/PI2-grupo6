bfc2ec19c903e573975dd7555e656cf0908168ff
Pablo Alvarez <pipualvarez1@gmail.com>
2024-02-20 21:20:44 -0300

    fix things

--- front-end/src/app/admin/forms/CreateUser.tsx
+++ front-end/src/app/admin/forms/CreateUser.tsx
@@ -50,7 +50,7 @@ const CreateUser = () => {
                     </div>
                 ))}
                 </div>
-                <button type="submit" className="text-primary-50 p-1 px-3 text-sm bg-baltic-sea-800 w-fit self-center rounded-2xl font-squada border-2 border-baltic-sea-900 drop-shadow-md active:scale-95">Alta Socio</button>
+                <ButtonForm text={"Alta socio"}/>
             </form>
             <DevTool control={control} />
         </section>
